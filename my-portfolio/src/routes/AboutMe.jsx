6ebf39acccfba0f78cbd52ff9fb7220e033be000
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-08-15 17:36:45 -0300

    Tudo certo

--- my-portfolio/src/routes/AboutMe.jsx
+++ my-portfolio/src/routes/AboutMe.jsx
@@ -18,32 +18,31 @@ const AboutMe = () => {
                     <ul className="ul-projects">
                         <li>Reac Services</li>
                         <p>Um CRUD para fazer solicitação de serviços para veículo, você cadastra seus dados e de acordo com seu orçamento pode solicitar serviços para o seu veículo. Utiliza react router dom, axios react tostify, no backend NodeJs, mongoose, express, bcrypt, jsonwebtoken e mongoDB Atlas.</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/ReactServices" target="_blank">Aqui</a></p>
 
                         <li>React Recipes </li>
                         <p>Um CRUD para fazer postagem de receitas, você cadastra seus dados e pode postar, editar e excluir suas receitas e ver as receitas postadas por outras pessoas, tem upload de imagem através de arquivo do computador. Utiliza react router dom, axios react tostify, no backend NodeJs, mongoose, express, bcrypt, jsonwebtoken, multer e mongoDB Atlas.</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/ReactRecipes" target="_blank">Aqui</a></p>
 
                         <li>React Quiz</li>
                         <p>Um quiz feito em react, utilizando useReducer e o useContext</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/ReactQuiz" target="_blank">Aqui</a></p>
 
                         <li>Github Finder com react e TypeScript</li>
                         <p>Um sistema para procurar perfis do Github e retorna o perfil com os melhores repositórios. Utiliza o react router dom e api do Github.</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
-
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/GitHubFinderReactTS" target="_blank">Aqui</a></p>
 
                         <li>React Party Time</li>
                         <p>Um sistema para agendamento de festas, cria, edita e mostra as festas cadastradas. Utilizei React com react-router-dom, axios e no backend NodeJS com express, mongoose e mongoDB Atlas.</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/ReactPartyTime" target="_blank">Aqui</a></p>
 
                         <li>React Memories</li>
                         <p>Um CRUD para guardar memórias, consegue fazer o upload de imagem através de arquivos. Utilizei React com react-router-dom, axios e no backend NodeJS com express, mongoose, mongoDB Atlas e o multer.</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/ReactMemories" target="_blank">Aqui</a></p>
 
                         <li>Formulário multistep com React e TypeScript</li>
                         <p>Um formulário para avaliações dos usuários.</p>
-                        <p>Repoitório <a className="link" href="#">Aqui</a></p>
+                        <p>Repoitório <a className="link" href="https://github.com/vitoriafreitasdev/FormularioMultistepReactTS" target="_blank">Aqui</a></p>
 
 
                     </ul>
