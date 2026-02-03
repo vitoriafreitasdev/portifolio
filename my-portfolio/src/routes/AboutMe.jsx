4bcbc5979ba22afa641e1a69363e722a24202bf0
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-02-03 08:14:39 -0300

    mudanças na pagina de projetos pessoais

--- my-portfolio/src/routes/AboutMe.jsx
+++ my-portfolio/src/routes/AboutMe.jsx
@@ -1,9 +1,10 @@
+
 import certificadoAlgoritmos from "../../images/certificado1.png"
 import certificadoFrontEnd from "../../images/certificado2.png"
 
 import "./AboutMe.css"
 const AboutMe = () => {
-    // fazer os repositorios no git de cada um dos projetos.
+    
   return (
     <div className="aboutme-main-div">
         
@@ -13,43 +14,52 @@ const AboutMe = () => {
             <div className="div-projects">
                     <h2>Projetos pessoais.</h2>
                     <p className="introduction-p">
-                        Desde que iniciei os estudos fiz alguns projetos pessoais com o conhecimento que tinha na época que fiz ele, abaixo estão elas:
+                        Cada projeto representa um marco na minha jornada de aprendizado, 
+                        utilizando as tecnologias que dominava em cada fase.
                     </p>
 
                     <ul className="ul-projects">
                         <li>React platarforma de gestão de estoque</li>
-                        <p>Uma simples platarforma de gestão de estoque feita com React JS, NodeJs. Nessa platarforma usuários podem se cadastrar para pedir produtos, se disponível. Administradores podem se cadastrar se possuírem a chave de acesso para administradores, eles podem editar, postar e excluir produtos. Utiliza react router dom, axios, no backend NodeJs, mongoose, express, bcrypt, jsonwebtoken, multer e mongoDB Atlas.</p>
+                        <p>Uma plataforma completa de gestão de estoque com autenticação de usuários e administradores. Usuários podem solicitar produtos disponíveis, enquanto administradores (com chave de acesso) podem gerenciar todo o inventário.</p>
+                        <p><span>Tecnologias:</span> React (react router dom, axios), Node.js, MongoDB, Express, JWT, Multer</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/platarforma-de-gestao-de-estoque" target="_blank">Aqui</a></p>
                         
                         <li>Quiz de JS + React e Python</li>
                         <p>
-                            Um quiz que o usuário pode escolher entre fazer de JS + React ou de Python. Usuário faz o seu cadastro, assim tendo acesso aos dois quiz e a sua maior/atual pontuação. No fim do quiz pode acessar as maiores pontuações entre os usuários cadastrados. Feito com React no front-end e NodeJS e MongoDB no back-end, esse projeto está disponível online <a className="link" href="https://quiz-redux-extension-frontend.vercel.app/" target="_blank">neste link</a>:
+                           Plataforma de quiz com duas categorias técnicas. Usuários cadastrados podem testar seus conhecimentos, competir por pontuações e ver um ranking global.
+                           Disponível: <a className="link" href="https://quiz-redux-extension-frontend.vercel.app/" target="_blank">neste link</a>:
                         </p>
+                        <p><span>Tecnologias:</span> React (react router dom, axios, redux), Redux, Node.js, MongoDB, Express</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/quiz_redux_extension" target="_blank">Aqui</a></p>
+
                         <li>React Services</li>
-                        <p>Um CRUD para fazer solicitação de serviços para veículo, você cadastra seus dados e de acordo com seu orçamento pode solicitar serviços para o seu veículo, possui um painel administrativo, apenas o usuário administrador tem acesso. Utiliza react router dom, axios react tostify, no backend NodeJs, mongoose, express, bcrypt, jsonwebtoken e mongoDB Atlas.</p>
+                        <p>Sistema para solicitação de serviços automotivos com base no orçamento do cliente. Inclui painel administrativo exclusivo para gestão de pedidos.</p>
+                        <p><span>Tecnologias:</span> React (react router dom, toastify), Node.js, MongoDB, Express, JWT</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/ReactServices" target="_blank">Aqui</a></p>
 
                         <li>React Recipes </li>
-                        <p>Um CRUD para fazer postagem de receitas, você cadastra seus dados e pode postar, editar e excluir suas receitas e ver as receitas postadas por outras pessoas, tem upload de imagem através de arquivo do computador. Utiliza react router dom, axios react tostify, no backend NodeJs, mongoose, express, bcrypt, jsonwebtoken, cloudinary e mongoDB Atlas. Deploy feito pelo Vercel, disponível: <a href="https://react-recipes-frontend-zeta.vercel.app/" target="_black">Aqui</a></p>
+                        <p>Comunidade para compartilhamento de receitas culinárias. Usuários podem postar, editar e explorar receitas de outros, com suporte a upload de fotos, disponível: <a href="https://react-recipes-frontend-zeta.vercel.app/" target="_black">Aqui</a></p>
+                        <p><span>Tecnologias:</span> React (react router dom, axios, toastify), Node.js, Cloudinary, MongoDB, Express</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/ReactRecipes" target="_blank">Aqui</a></p>
 
-                        <li>JS puro - Site para registro de treinos</li>
-                        <p>Um CRUD para fazer postagem de treinos, usuário cadastrado pode postar seus treinos da semana. Utiliza JavaScript no front-end, no back-end utiliza NodeJS com express e mongoose, junto com JWT e bcrypt. Para armazenamento de dados usa MongoDB Atlas.</p>
+                        <li>Gerenciador de Treinos</li>
+                        <p>Aplicação para registro e acompanhamento de treinos semanais. Usuários organizam sua rotina de exercícios de forma simples e intuitiva.</p>
+                        <p><span>Tecnologias:</span> JavaScript, Node.js, MongoDB, Express, JWT</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/JS_Project" target="_blank">Aqui</a></p>
 
-                        <li>Projeto de React + TypeScript com Redux Toolkit</li>
-                        <p>Um projeto que utiliza Redux Toolkit, utiliza uma API pública. Fez requisição para busca de personagens dentro dessa API usando o createAsyncThunk do Redux Toolkit, lógica de filtragem dos personagens pelo nome, adição e remoção de personagens favoritos acontece pelo reducer criado com Redux Toolkit.</p>
+                        <li>React + TypeScript com Redux Toolkit</li>
+                        <p>Aplicação que consome API pública para busca e gerenciamento de personagens. Demonstração avançada do uso de Redux Toolkit com operações assíncronas.</p>
+                        <p><span>Tecnologias:</span> React, TypeScript, Redux Toolkit, API REST</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/ReduxProject" target="_blank">Aqui</a></p>
 
-                        <li>React Quiz</li>
+                        <li>Github Finder</li>
+                        <p>Um sistema para procurar perfis do Github e retorna o perfil com os melhores repositórios. Utiliza o react router dom e api do Github.</p>
+                        <p><span>Tecnologias:</span> React, TypeScript, GitHub API</p>
+                        <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/GitHubFinderReactTS" target="_blank">Aqui</a></p>
+
+                        {/* <li>React Quiz</li>
                         <p>Um quiz feito em react, utilizando useReducer e o useContext</p>
                         <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/ReactQuiz" target="_blank">Aqui</a></p>
-                        
-
-                        <li>Github Finder com react e TypeScript</li>
-                        <p>Um sistema para procurar perfis do Github e retorna o perfil com os melhores repositórios. Utiliza o react router dom e api do Github.</p>
-                        <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/GitHubFinderReactTS" target="_blank">Aqui</a></p>
 
                         <li>React Party Time</li>
                         <p>Um sistema para agendamento de festas, cria, edita e mostra as festas cadastradas. Utilizei React com react-router-dom, axios e no backend NodeJS com express, mongoose e mongoDB Atlas.</p>
@@ -65,7 +75,7 @@ const AboutMe = () => {
 
                         <li>APIRESTful com nodeJS.</li>
                         <p>Uma api feita com nodejs para um CRUD simples, utiliza nodeJS com Express e mongoose para fazer as interações com o banco MongoDB Atlas.</p>
-                        <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/API-RESTful-NodeJS" target="_blank">Aqui</a></p>
+                        <p>Repositório <a className="link" href="https://github.com/vitoriafreitasdev/API-RESTful-NodeJS" target="_blank">Aqui</a></p> */}
                     </ul>
                     
             </div>
